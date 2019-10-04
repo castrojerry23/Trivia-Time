b7e27e5278fdde9fbe7a2eae8f3e8063d3f4a0b2
Jerry Castro <castrojerry23@gmail.com>
2019-10-04 14:14:20 -0500

    changed background photo and created trivia question and answers

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,23 @@
+const listOfQuestions = [
+    {question: "To get over Richard, what did Monica start making?", answer1: "Marmalade", answer2: "Jam", answer3: "Pancakes", answer4: "Candy", finalAttr: "answer2", finalText: "Jam!"},
+    {question: "Which store did Rachel and Ross both buy the same apothecary table from?", answer1: "Pottery Barn", answer2: "West Elm", answer3: "Anthropology", answer4: "Crate & Barrel", finalAttr: "answer1", finalText: "Pottery Barn!"},
+    {question: "Monica's apartment number was originally 5. What number was it later changed to for the rest of the series?", answer1: "15", answer2: "7", answer3: "20", answer4: "3", finalAttr: "answer3", finalText: "20!"},
+    {question: "In 'The One Where No Ones Ready,' what does Phoebe spill on her dress?", answer1: "Wine", answer2: "Hummus", answer3: "French Onion Dip", answer4: "Guacamole", finalAttr: "answer2", finalText: "Hummus!"},
+    {question: "How long is the letter Rachel writes Ross at the beach house?", answer1: "10 pages, front and back", answer2: "10 pages", answer3: "18 pages, front and back", answer4: "18 pages", finalAttr: "answer3", finalText: "18 pages, front and back!"},
+    {question: "What is the reason that Phoebe tells Mike's mom for her getting hepatitis?", answer1: "A pimp spit in her mouth", answer2: "Her mother passed it to her during birth", answer3: "Sharing a toothbrush when she was homeless", answer4: "She was bit by homeless person", finalAttr: "answer1", finalText: "A pimp spit in her mouth!"},
+    {question: "Who plays Ben, Ross' son?", answer1: "Dylan Sprouse", answer2: "Zac Effron", answer3: "Macaulay Culkin", answer4: "Cole Sprouse", finalAttr: "answer4", finalText: "Cole Sprouse!"},
+    {question: "What was the name of Ross and Chandler's college band?", answer1: "Emotional Knapsack", answer2: "She Feels Weird Since I've Been Gone", answer3: "Betrayal in The Common Room", answer4: "Way, No Way", finalAttr: "answer4", finalText: "Way, Now Way!"},
+    {question: "Where does Pete take Monica for their first date?", answer1: "The Cheesecake Factory", answer2: "Greece", answer3: "Central Perk", answer4: "Paris", finalAttr: "answer2", finalText: "Greece!"},
+    {question: "What is the name of the 1950s-themed diner where Monica worked as a waitress?", answer1: "Marilyn & Audrey", answer2: "Twilight Galaxy", answer3: "Moondance Diner", answer4: "Hard Rock Cafe", finalAttr: "answer3", finalText: "Moondance Diner"},
+    {question: "What is the name of of Joey's stuffed penguin?", answer1: "Huggsy", answer2: "Snowflake", answer3: "Waddle", answer4: "Snuggles", finalAttr: "answer1", finalText: "Huggsy!"},
+    {question: "What instrument does Phoebe play?", answer1: "Piano", answer2: "Drums", answer3: "Flute", answer4: "Guitar", finalAttr: "answer4", finalText: "Guitar"},
+    {question: "What does Joey not share with anyone?", answer1: "Food", answer2: "Money", answer3: "Beer", answer4: "Bed", finalAttr: "answer1", finalText: "Food!"},
+    {question: "What is Chandler's middle name?", answer1: "Stacey", answer2: "Casey", answer3: "Muriel", answer4: "Marie", finalAttr: "answer3", finalText: "Muriel!"},
+    {question: "What is the name of Phoebe's alter ego?", answer1: "Elaine Benes", answer2: "Janet Snakehole", answer3: "Ursula Buffay", answer4: "Regina Falange", finalAttr: "answer4", finalText: "Regina Falange!"},
+    {question: "What hangs item hangs on Monica's front door?", answer1: "Cross", answer2: "A yellow picture frame", answer3: "Coat hook", answer4: "Calandar", finalAttr: "answer2", finalText: "A yellow picture frame!"},
+    {question: "What is Ross' profession?", answer1: "Lawyer", answer2: "Data Analyst", answer3: "Medical Doctor", answer4: "Paleontologist", finalAttr: "answer4", finalText: "Paleontologist!"},
+    {question: "What is Phoebe's most famous song?", answer1: "Double-Jointed Boy", answer2: "Sticky Shoes", answer3: "Smelly Cat", answer4: "Ode to a Pubic Hair", finalAttr: "answer3", finalText: "Smelly Cat!"},
+    {question: "What is the name of the coffee shop that the characters would frequently go to?", answer1: "Insomnia Cafe", answer2: "Central Perk", answer3: "Box Kite", answer4: "Little Collins", finalAttr: "answer2", finalText: "Central Perk!"},
+    {question: "What fictional character does Rachel dress up as to impress Ross?", answer1: "Princess Leia", answer2: "Xena", answer3: "Wonder Woman", answer4: "Lara Croft", finalAttr: "answer1", finalText: "Princess Leia!"}
+]
+
